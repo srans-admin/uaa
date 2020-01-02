c3028a5a8762bd8a55eda718fc84171bbe3fed8a
manishk219009 <42214844+manishk219009@users.noreply.github.com>
2020-01-02 21:48:27 +0530

    UAA Updated For Notification API

--- src/main/java/com/srans/uaa/controller/UserSubscriptionController.java
+++ src/main/java/com/srans/uaa/controller/UserSubscriptionController.java
@@ -1,6 +1,7 @@
 package com.srans.uaa.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,57 +15,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.srans.uaa.domain.Subscription;
 import com.srans.uaa.domain.User;
 import com.srans.uaa.domain.UserSubscription;
 import com.srans.uaa.oauth2service.SubscriptionService;
 import com.srans.uaa.oauth2service.UserService;
+import com.srans.uaa.repository.UserRepository;
 
 @RestController
 public class UserSubscriptionController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private SubscriptionService subscriptionService;
-	
 
-    @GetMapping("/user/me")
-    public Principal user(Principal principal) {
-        return principal;
-    }
-    
-    @PostMapping("/v1/users")
-    public ResponseEntity<UserSubscription> addUserWithSubscription(@RequestBody UserSubscription userSubscription) {
-    	
-    	UserSubscription userSubscriptionNew =  new UserSubscription();
-    	userSubscriptionNew.setUser(userService.save(userSubscription.getUser()));
-    	userSubscriptionNew.setSubscription(subscriptionService.save(userSubscription.getSubscription()));
-    	
-    	return new ResponseEntity<UserSubscription>(userSubscriptionNew, new HttpHeaders(), HttpStatus.OK);
-    }
-    
-    @GetMapping("/v1/users")
-    public ResponseEntity<List<User>> getUsers() {
-    	
-    	List<User> users = userService.getAll(); 
-    	return new ResponseEntity<List<User>>(users, new HttpHeaders(), HttpStatus.OK);
-    }
-    
-    @GetMapping("/v1/users/{id}")
-    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
-    	
-    	User usernew = userService.get(id); 
-    	return new ResponseEntity<User>(usernew, new HttpHeaders(), HttpStatus.OK);
-    }
-    
-    @DeleteMapping("/v1/users/{id}")
-    public ResponseEntity<Boolean> deleteUser(@PathVariable("id") Long id) {
-    	
-    	userService.delete(id); 
-    	boolean isDone = true;
-    	return new ResponseEntity<Boolean>(isDone, new HttpHeaders(), HttpStatus.OK);
-    }
-    
+	@Autowired
+	private UserRepository userRepo;
+
+	@GetMapping("/user/me")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@PostMapping("/v1/users")
+	public ResponseEntity<UserSubscription> addUserWithSubscription(@RequestBody UserSubscription userSubscription) {
+
+		UserSubscription userSubscriptionNew = new UserSubscription();
+		userSubscriptionNew.setUser(userService.save(userSubscription.getUser()));
+		userSubscriptionNew.setSubscription(subscriptionService.save(userSubscription.getSubscription()));
+
+		return new ResponseEntity<UserSubscription>(userSubscriptionNew, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@GetMapping("/v1/users")
+	public ResponseEntity<List<User>> getUsers() {
+
+		List<User> users = userService.getAll();
+		return new ResponseEntity<List<User>>(users, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@GetMapping("/v1/users/{id}")
+	public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+
+		User usernew = userService.get(id);
+		return new ResponseEntity<User>(usernew, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/v1/users/{id}")
+	public ResponseEntity<Boolean> deleteUser(@PathVariable("id") Long id) {
+
+		userService.delete(id);
+		boolean isDone = true;
+		return new ResponseEntity<Boolean>(isDone, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@GetMapping("v1/superAdminUserName")
+	public Object[] getSuperAdminUserName() {
+
+		
+		Object[] superAdmin = userRepo.findUsernameByRole("superadmin".toUpperCase());
+		return superAdmin;
+	}
+
 }
--- src/main/java/com/srans/uaa/repository/UserRepository.java
+++ src/main/java/com/srans/uaa/repository/UserRepository.java
@@ -1,18 +1,23 @@
 package com.srans.uaa.repository;
  
-import org.springframework.stereotype.Repository;
-
-import com.srans.uaa.domain.User;
-
+import java.util.List;
 import java.util.Optional;
+
+import javax.xml.ws.Response;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.srans.uaa.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 	
 	@Query("from User s where s.username=:username ")
 	public Optional<User> findByUserName(@Param("username") String username);
+	
+	@Query(value = "select username from user  where  role=?1", nativeQuery = true)
+	 public Object[] findUsernameByRole(String role);
 }
